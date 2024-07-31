2ae1b16a06eabb4b36ceb7bb8ae93349a586b704
cinta <naurah_zanjabila_33rpl@student.smktelkom-mlg.sch.id>
2024-07-31 13:13:56 +0700

    Add files via upload

---
+++ Tes.java
@@ -0,0 +1,7 @@
+public class Tes {
+    public static void main(String[] args) {
+        String nama = "Cinta" ;
+        String kelas = "X RPL 5" ;
+        System.out.print("Halo, nama saya " + cinta + " dari kelas " + X RPL 5);
+    }
+}
